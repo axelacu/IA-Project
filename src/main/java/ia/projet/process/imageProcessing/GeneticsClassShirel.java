ae2415c2cd959b659876565b74251f7823c486bc
axel_ <axel.acuna@dauphine.eu>
2018-11-18 17:51:42 +0100

    Test commit axel 3

--- src/main/java/ia/projet/process/imageProcessing/GeneticsClassShirel.java
+++ src/main/java/ia/projet/process/imageProcessing/GeneticsClassShirel.java
@@ -2,10 +2,5 @@ package ia.projet.process.imageProcessing;
 
 public class GeneticsClassShirel {
     int variable;
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
 }
 
