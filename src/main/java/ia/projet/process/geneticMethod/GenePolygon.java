b013fd526341496071a0d64513e7de18f0c365f5
axel_ <axel.acuna@dauphine.eu>
2018-11-20 18:03:44 +0100

    Completing GenePolygon class,[NOT FINISH] TODO : DEEP COPY CONSTRUCTOR, AND IMPROVE MUTATION METHOD.

--- src/main/java/ia/projet/process/geneticMethod/GenePolygon.java
+++ src/main/java/ia/projet/process/geneticMethod/GenePolygon.java
@@ -1,8 +1,32 @@
 package ia.projet.process.geneticMethod;
 
-public class GenePolygon implements Gene{
+import ia.projet.process.imageProcessing.ConvexPolygon;
+import javafx.scene.paint.Paint;
+
+public class GenePolygon extends ConvexPolygon implements Gene{
+    private int numberOfPoints;
+    private Paint color;
+    //TODO : Add arrayPoints. ajouter
+
+    public GenePolygon(int numberOfPoints){
+        super(numberOfPoints);
+        this.numberOfPoints = numberOfPoints;
+        color = this.getFill();
+    }
+
+    public GenePolygon(GenePolygon genePolygon){}
+
+    /**
+     * Return gene who should mutate
+     * @return a gene mutate
+     */
     @Override
     public GenePolygon mutation() {
-        return null;
+        return new GenePolygon(numberOfPoints);
     }
+
+    public int getNumberOfPoints() {
+        return numberOfPoints;
+    }
+
 }
