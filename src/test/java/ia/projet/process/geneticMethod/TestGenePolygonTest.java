5d80b561de5ef24709dd8d2006f57cc20a58cd63
Shirel Matti <shirel.matti@dauphine.eu>
2018-11-27 18:48:11 +0100

    TestMutation do and pass

--- src/test/java/ia/projet/process/geneticMethod/TestGenePolygonTest.java
+++ src/test/java/ia/projet/process/geneticMethod/TestGenePolygonTest.java
@@ -23,9 +23,35 @@ public class TestGenePolygonTest  extends TestCase {
         GenePolygon genePolygon1=new GenePolygon(4);
         GenePolygon genepolygone2=genePolygon1.mutation();
         assertTrue(genePolygon1!=genepolygone2);
+        assertTrue(genePolygon1.getGeneById()!=genepolygone2.getGeneById());
+        assertTrue(!(genePolygon1.equals(genepolygone2)));;
+    }
+
+    public static void testMutationPoint() {
+        ConvexPolygon.max_X=100;
+        ConvexPolygon.max_Y=140;
+        GenePolygon genePolygon1=new GenePolygon(4);
+        GenePolygon genepolygone2=genePolygon1.mutation();
+        assertFalse(genePolygon1.equals(genepolygone2));
         assertTrue(!(genePolygon1.equals(genepolygone2)));
+        assertFalse(genePolygon1==genepolygone2);
+
+        GenePolygon genePolygon3=genePolygon1.mutationFill();
+        assertFalse(genePolygon1.getFill()==genePolygon3.getFill());
+        GenePolygon genePolygon4=genePolygon1.mutationOpacity();
+        assertFalse(genePolygon1.getOpacity()==genePolygon4.getOpacity());
+        assertFalse(genePolygon1==genePolygon3);
+        assertFalse(genePolygon1==genePolygon4);
+        assertFalse(genePolygon1.equals(genePolygon3));
+        assertFalse(genePolygon1.equals(genePolygon4));
+        GenePolygon genePolygon5=genePolygon1.mutationRotation();
+        assertFalse(genePolygon1==genePolygon5);
+
+
 
     }
+
+
 
 
 }
